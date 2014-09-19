05dea0761b7aa036ec28267817ab7192d2d5b9a9
austin brown <austinjbraun@gmail.com>
2014-09-18 21:54:41 -0700

    completed wrapping api get calls to athlete/athlets endpoints along w/ pagination

--- test/strava_athlete.js
+++ test/strava_athlete.js
@@ -1,19 +1,20 @@
 
-var assert = require("assert");
-var strava = require("../");
+var should = require("should")
+    , strava = require("../");
 
-var athlete_id = "5256054";
+var athlete_id = "2402496";
 
 describe('strava', function(){
 
-    describe('#getCurrentAthlete()', function() {
+    describe('#getCurrent()', function() {
 
-        it('should return detailed athlete information about athlete associated to access_token', function(done) {
+        it('should return detailed athlete information about athlete associated to access_token (level 3)', function(done) {
 
-            strava.athlete.getCurrentAthlete(null,function(err,payload){
+            strava.athlete.getCurrent({},function(err,payload){
 
                 if(!err) {
-                    console.log(payload);
+                    //console.log(payload);
+                    (payload.resource_state).should.be.exactly(3);
                 }
                 else {
                     console.log(err);
@@ -24,13 +25,15 @@ describe('strava', function(){
         });
     });
 
-    describe.only('#getAthlete()',function(){
-       it('should return detailed athlete information', function(done){
+    describe('#listCurrentFriends()', function() {
 
-            strava.athlete.getAthlete({id:athlete_id},function(err,payload){
+        it('should return information about friends associated to athlete with access_token', function(done) {
+
+            strava.athlete.listCurrentFriends({},function(err,payload){
 
                 if(!err) {
-                    console.log(payload);
+                    //console.log(payload);
+                    payload.should.be.instanceof(Array);
                 }
                 else {
                     console.log(err);
@@ -38,6 +41,101 @@ describe('strava', function(){
 
                 done();
             });
-       })
+        });
+    });
+
+    describe('#listCurrentFollowers()', function() {
+
+        it('should return information about followers associated to athlete with access_token', function(done) {
+
+            strava.athlete.listCurrentFollowers({},function(err,payload){
+
+                if(!err) {
+                    //console.log(payload);
+                    payload.should.be.instanceof(Array);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        });
+    });
+
+    describe('#get()',function(){
+        it('should return basic athlete information (level 2)', function(done){
+
+            strava.athlete.get({id:athlete_id},function(err,payload){
+
+                if(!err) {
+                    //console.log(payload);
+                    (payload.resource_state).should.be.exactly(2);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        })
+    });
+
+    describe('#listFriends()',function(){
+        it('should return information about friends associated to athlete id', function(done){
+
+            strava.athlete.listFriends({id:athlete_id},function(err,payload){
+
+                if(!err) {
+                    //console.log(payload);
+                    payload.should.be.instanceof(Array);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        })
+    });
+
+    describe('#listFollowers()',function(){
+        it('should return information about followers associated to athlete id', function(done){
+
+            strava.athlete.listFollowers({id:athlete_id},function(err,payload){
+
+                if(!err) {
+                    //console.log(payload);
+                    payload.should.be.instanceof(Array);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        })
+    });
+
+    describe('#getKoms()',function(){
+        it('should return detailed athlete information', function(done){
+
+            strava.athlete.getKoms({
+                id:athlete_id
+                , page:1
+                , per_page:2
+            },function(err,payload){
+
+                if(!err) {
+                    //console.log(payload);
+                    payload.should.be.instanceof(Array);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        })
     });
 });
