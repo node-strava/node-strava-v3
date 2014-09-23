41b487419a325cf9e11e6a050766f6486c537dbb
austin brown <austinjbraun@gmail.com>
2014-09-23 12:07:21 -0700

    added support for segments endpoint

--- test/segments.js
+++ test/segments.js
@@ -8,7 +8,7 @@ describe('segments', function() {
 
     describe('#get()', function () {
 
-        it('should return detailed athlete information about segment (level 3)', function (done) {
+        it('should return detailed information about segment (level 3)', function (done) {
 
             strava.segments.get({id:segment_id}, function (err, payload) {
 
@@ -63,15 +63,42 @@ describe('segments', function() {
         });
     });
 
-    describe.only('#listLeaderboard()', function () {
+    describe('#listLeaderboard()', function () {
 
         it('should list leaderboard for segment', function (done) {
 
-            strava.segments.listLeaderboard({id:segment_id,page:1,per_page:4,gender:"F"}, function (err, payload) {
+            strava.segments.listLeaderboard({
+                id:segment_id
+                ,page:1
+                ,per_page:4
+                ,gender:"M"
+            }, function (err, payload) {
 
                 if (!err) {
-                    console.log(payload);
-                    payload.should.be.instanceof(Array);
+                    //console.log(payload);
+                    payload.entries.should.be.instanceof(Array);
+                }
+                else {
+                    console.log(err);
+                }
+
+                done();
+            });
+        });
+    });
+
+    describe('#explore()', function () {
+
+        it('should return up to 10 segments w/i the given bounds', function (done) {
+
+            strava.segments.explore({
+                bounds:"37.821362,-122.505373,37.842038,-122.465977"
+                , activity_type: "running"
+            }, function (err, payload) {
+
+                if (!err) {
+                    //console.log(payload);
+                    payload.segments.should.be.instanceof(Array);
                 }
                 else {
                     console.log(err);
