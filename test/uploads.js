e83cfb0b3e6058e900dacd9c392ede3a5804b84d
austin brown <austinjbraun@gmail.com>
2016-05-26 00:32:34 -0700

    Update mocha+increase upload timeout

--- test/uploads.js
+++ test/uploads.js
@@ -7,9 +7,10 @@ require('es6-promise').polyfill();
 var should = require("should")
     , strava = require("../");
 
-describe('uploads_test', function(){
+describe.only('uploads_test', function(){
     describe('#post()', function() {
         it('should upload a GPX file', function(done) {
+            this.timeout(30000);
             new Promise(function (resolve, reject) {
               strava.uploads.post({
                   activity_type:'run'
@@ -17,11 +18,11 @@ describe('uploads_test', function(){
                   , name:'test activity'
                   , file:'test/assets/gpx_sample.gpx'
                   , statusCallback: function(err,payload) {
-                    should.not.exist(err)
-                    should.not.exist(payload.error)
+                    should.not.exist(err);
+                    should.not.exist(payload.error);
 
                     if(payload.activity_id === null) {
-                        (payload.status).should.be.exactly('Your activity is still being processed.')
+                        (payload.status).should.be.exactly('Your activity is still being processed.');
                     } else {
                         (payload.status).should.be.exactly('Your activity is ready.');
                         (payload.activity_id).should.be.a.Number;
