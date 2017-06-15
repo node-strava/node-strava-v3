aba1fe3b8e249a7d94778875341db380ea1a0ec3
Mark Stosberg <mark@rideamigos.com>
2017-06-15 13:41:14 -0400

    BugFix: Don't send Authorization header with client id / client secret auth is used.
    This could cause Strava to return an error. Plus, it was confusing to
    mix the two auth styles.

--- test/pushSubscriptions.js
+++ test/pushSubscriptions.js
@@ -15,6 +15,11 @@ describe("pushSubscriptions_test", function() {
      stravahost = nock('https://api.strava.com')
                      .filteringPath(() => '/api/v3/push_subscriptions/')
                      .get(/^\/api\/v3\/push_subscriptions/)
+                     // The first reply just echo's the request headers so we can test them.
+                     .reply(200,function(uri, requestBody) {
+                        return { headers: this.req.headers };
+                     })
+                     .get(/^\/api\/v3\/push_subscriptions/)
                      .reply(200, [
                          {
                            "id": 1,
@@ -26,6 +31,13 @@ describe("pushSubscriptions_test", function() {
                          }
                      ]);
     })
+
+    it("should not sent Authorization header to Strava", () => {
+      return strava.pushSubscriptions.list()
+        .then(result => {
+          should.not.exist(result.headers.authorization);
+        });
+    });
 
     it("should return list of subscriptions", () => {
       return strava.pushSubscriptions.list()
@@ -50,6 +62,11 @@ describe("pushSubscriptions_test", function() {
      before(() => {
        var stravahost = nock('https://api.strava.com')
                        .filteringPath(() => '/api/v3/push_subscriptions')
+                       // The first reply just echo's the request headers so we can test them.
+                       .post(/^\/api\/v3\/push_subscriptions/)
+                       .reply(200,function(uri, requestBody) {
+                          return { headers: this.req.headers };
+                       })
                        .post(/^\/api\/v3\/push_subscriptions/)
                        .reply(200, {
                              "id": 1,
@@ -65,6 +82,15 @@ describe("pushSubscriptions_test", function() {
     it("should throw with no params", () => {
       assert.throws(() => strava.pushSubscriptions.create())
     })
+
+    it("should not sent Authorization header to Strava", () => {
+      return strava.pushSubscriptions.create({
+          "callback_url": "http://you.com/callback/",
+        })
+        .then(result => {
+          should.not.exist(result.headers.authorization);
+        });
+    });
 
     it("should return details of created activity", () => {
       return strava.pushSubscriptions.create({
@@ -91,6 +117,11 @@ describe("pushSubscriptions_test", function() {
       // We return it here because the test can't easily access the HTTP status code.
        var stravahost = nock('https://api.strava.com')
                        .filteringPath(() => '/api/v3/push_subscriptions/1/')
+                       // The first reply just echo's the request headers so we can test them.
+                       .delete(/^\/api\/v3\/push_subscriptions\/1/)
+                       .reply(200,function(uri, requestBody) {
+                          return { headers: this.req.headers };
+                       })
                        .delete(/^\/api\/v3\/push_subscriptions\/1/)
                        .reply(204,function(uri, requestBody) {
                           requestBody = JSON.parse('{"status":204}');
@@ -101,6 +132,13 @@ describe("pushSubscriptions_test", function() {
     it("should throw with no id", () => {
       assert.throws(() => strava.pushSubscriptions.delete())
     })
+
+    it("should not sent Authorization header to Strava", () => {
+      return strava.pushSubscriptions.delete({id:1})
+        .then(result => {
+          should.not.exist(result.headers.authorization);
+        });
+    });
 
     it("Should return 204 after successful delete", () => {
       return strava.pushSubscriptions.delete({id:1})
