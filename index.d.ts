8bf4628826c07987acf9398481722a02c597e0a9
ntziolis <nikolas@tziolis.de>
2020-10-17 08:24:56 +0200

    fix(typings): revert to previous typings

--- index.d.ts
+++ index.d.ts
@@ -155,6 +155,5 @@ export interface Strava {
     oauth: OAuthRoutes;
 }
 
-declare const strava : Strava;
-export = strava;
-export as namespace strava;
+export const strava: Strava;
+export default strava;
