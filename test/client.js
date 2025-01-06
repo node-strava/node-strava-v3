446aaeb11d83bf348ea0beef78914b286e73d900
Kyle-Ski <skiroyjenkins@gmail.com>
2025-01-06 12:31:21 -0800

    client and clubs tests done, WIP: gear, oauth, pushSubscriptions, segmentEfforts and uploads

--- test/client.js
+++ test/client.js
@@ -16,33 +16,30 @@ describe('client_test', function () {
   // All data fetching methods should work on the client (except Oauth).
   // Try client.athlete.get() as a sample
   describe('#athlete.get()', function () {
-    it('Should reject promise with StatusCodeError for non-2xx response', function (done) {
+    it('Should reject promise with StatusCodeError for non-2xx response', async function (done) {
       const badClient = new strava.client('BOOM')
-      badClient.athlete.get({})
-        .then(() => {
-          // If the promise resolves, the test should fail
-          done(new Error('Expected get() to throw StatusCodeError'))
-        })
-        .catch(e => {
-          // Assert that the error is an instance of StatusCodeError
-          e.should.be.an.instanceOf(StatusCodeError)
-          done()
-        })
-        .catch(done) // Catch any unexpected errors in the catch block
+      try {
+        await badClient.athlete.get({})
+        // If we make it here, the call didn’t throw, so test fails
+        done(new Error('Expected get() to throw StatusCodeError'))
+      } catch (e) {
+        // Assert that the error is an instance of StatusCodeError
+        e.should.be.an.instanceOf(StatusCodeError)
+        done()
+      }
     })
 
-    it('should return detailed athlete information about athlete associated to access_token (level 3)', function (done) {
-      client.athlete.get({})
-        .then(payload => {
-          // Assert that the resource_state is exactly 3
-          payload.resource_state.should.be.exactly(3)
-          done()
-        })
-        .catch(err => {
-          // Log the error and fail the test
-          console.error(err)
-          done(err)
-        })
+    it('should return detailed athlete information about athlete associated to access_token (level 3)', async function (done) {
+      try {
+        const payload = await client.athlete.get({})
+        // Assert that the resource_state is exactly 3
+        payload.resource_state.should.be.exactly(3)
+        done()
+      } catch (err) {
+        // Log the error and fail the test
+        console.error(err)
+        done(err)
+      }
     })
   })
 })
--- test/clubs.js
+++ test/clubs.js
@@ -5,54 +5,52 @@ var testHelper = require('./_helper')
 var _sampleClub
 
 describe('clubs_test', function () {
-  before(function (done) {
-    testHelper.getSampleClub()
-      .then(function (payload) {
-        _sampleClub = payload
-        done()
-      })
-      .catch(done)
+  before(async function (done) {
+    try {
+      const payload = await testHelper.getSampleClub()
+      _sampleClub = payload
+      done()
+    } catch (err) {
+      done(err)
+    }
   })
 
   describe('#get()', function () {
-    it('should return club detailed information', function (done) {
-      strava.clubs.get({ id: _sampleClub.id })
-        .then(function (payload) {
-          payload.resource_state.should.be.exactly(3)
-          done()
-        })
-        .catch(function (err) {
-          console.log(err)
-          done(err)
-        })
+    it('should return club detailed information', async function (done) {
+      try {
+        const payload = await strava.clubs.get({ id: _sampleClub.id })
+        payload.resource_state.should.be.exactly(3)
+        done()
+      } catch (err) {
+        console.log(err)
+        done(err)
+      }
     })
   })
 
   describe('#listMembers()', function () {
-    it('should return a summary list of athletes in club', function (done) {
-      strava.clubs.listMembers({ id: _sampleClub.id })
-        .then(function (payload) {
-          payload.should.be.instanceof(Array)
-          done()
-        })
-        .catch(function (err) {
-          console.log(err)
-          done(err)
-        })
+    it('should return a summary list of athletes in club', async function (done) {
+      try {
+        const payload = await strava.clubs.listMembers({ id: _sampleClub.id })
+        payload.should.be.instanceof(Array)
+        done()
+      } catch (err) {
+        console.log(err)
+        done(err)
+      }
     })
   })
 
   describe('#listActivities()', function () {
-    it('should return a list of club activities', function (done) {
-      strava.clubs.listActivities({ id: _sampleClub.id })
-        .then(function (payload) {
-          payload.should.be.instanceof(Array)
-          done()
-        })
-        .catch(function (err) {
-          console.log(err)
-          done(err)
-        })
+    it('should return a list of club activities', async function (done) {
+      try {
+        const payload = await strava.clubs.listActivities({ id: _sampleClub.id })
+        payload.should.be.instanceof(Array)
+        done()
+      } catch (err) {
+        console.log(err)
+        done(err)
+      }
     })
   })
 })
