20e0fdec86f091765c97e0610346ae74c5e19564
Johan Cornelissen <johan1252@gmail.com>
2025-10-03 16:58:34 +1000

    Fix callback issues on Athlete and Uploads API
    - Moves fully over to async/await for Athlete and Uploads APIs.
    - Updates corresponding tests for Athlete and Uploads APIs
    - Updates Uploads tests to use latest datetime since deleting activities is not possible through Strava API anymore
    - Fixes issue where remenants of `requests` library was still uses in Uploads API. Resolves `this.request.post is not a function` error

--- test/athlete.js
+++ test/athlete.js
@@ -4,109 +4,61 @@ const testHelper = require('./_helper')
 
 describe('athlete_test', function () {
   describe('#get()', function () {
-    it('should return detailed athlete information about athlete associated to access_token (level 3)', function (done) {
-      strava.athlete.get({}, function (err, payload) {
-        if (!err) {
-          (payload.resource_state).should.be.exactly(3)
-        } else {
-          console.log(err)
-        }
-
-        done()
-      })
+    it('should return detailed athlete information about athlete associated to access_token (level 3)', async () => {
+      const response = await strava.athlete.get({})
+      response.resource_state.should.be.exactly(3)
     })
   })
 
   describe('#listActivities()', function () {
-    it('should return information about activities associated to athlete with access_token', function (done) {
+    it('should return information about activities associated to athlete with access_token', async () => {
       var nowSeconds = Math.floor(Date.now() / 1000)
-      strava.athlete.listActivities({
+      const response = await strava.athlete.listActivities({
         after: nowSeconds + 3600,
         before: nowSeconds + 3600
-      }, function (err, payload) {
-        if (!err) {
-          // console.log(payload);
-          payload.should.be.instanceof(Array)
-        } else {
-          console.log(err)
-        }
-
-        done()
       })
+      response.should.be.instanceof(Array)
     })
   })
 
   describe('#listClubs()', function () {
-    it('should return information about clubs associated to athlete with access_token', function (done) {
-      strava.athlete.listClubs({}, function (err, payload) {
-        if (!err) {
-          payload.should.be.instanceof(Array)
-        } else {
-          console.log(err)
-        }
-
-        done()
-      })
+    it('should return information about clubs associated to athlete with access_token', async () => {
+      const response = await strava.athlete.listClubs({})
+      response.should.be.instanceof(Array)
     })
   })
 
   describe('#listRoutes()', function () {
-    it('should return information about routes associated to athlete with access_token', function (done) {
-      strava.athlete.listRoutes({}, function (err, payload) {
-        if (!err) {
-          payload.should.be.instanceof(Array)
-        } else {
-          console.log(err)
-        }
-
-        done()
-      })
+    it('should return information about routes associated to athlete with access_token', async () => {
+      const response = await strava.athlete.listRoutes({})
+      response.should.be.instanceof(Array)
     })
   })
 
   describe('#listZones()', function () {
-    it('should return information about heart-rate zones associated to athlete with access_token', function (done) {
-      strava.athlete.listZones({}, function (err, payload) {
-        if (!err) {
-          payload.should.be.instanceof(Object)
-        } else {
-          console.log(err)
-        }
-
-        done()
-      })
+    it('should return information about heart-rate zones associated to athlete with access_token', async () => {
+      const response = await strava.athlete.listZones({})
+      response.should.be.instanceof(Object)
     })
   })
 
   describe('#update()', function () {
     // grab the athlete so we can revert changes
     var _athletePreEdit
-    before(function (done) {
-      testHelper.getSampleAthlete(function (err, payload) {
-        should(err).be.null()
-        _athletePreEdit = payload
-        done()
-      })
+    before(async () => {
+      _athletePreEdit = await testHelper.getSampleAthlete()
     })
 
-    it('should update the weight of the current athlete and revert to original', function (done) {
+    it('should update the weight of the current athlete and revert to original', async () => {
       var weight = 149
 
-      strava.athlete.update({ weight }, function (err, payload) {
-        if (!err) {
-          should(payload.weight).equal(weight)
+      const response = await strava.athlete.update({ weight })
+      should(response.weight).equal(weight)
 
-          // great! we've proven our point, let's reset the athlete data
-          strava.athlete.update({ city: _athletePreEdit.city }, function (err, payload) {
-            should(err).be.null()
-            should(payload.city).equal(_athletePreEdit.city)
-            done()
-          })
-        } else {
-          console.log(err)
-          done()
-        }
-      })
+      // great! we've proven our point, let's reset the athlete data
+      const updateResponse = await strava.athlete.update({ city: _athletePreEdit.city, weight: _athletePreEdit.weight })
+      should(updateResponse.city).equal(_athletePreEdit.city)
+      should(updateResponse.weight).equal(_athletePreEdit.weight)
     })
   })
 })
